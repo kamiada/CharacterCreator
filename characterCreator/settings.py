0a03a74c2c1784269dd792f9b9036a2fa52eae03
kamiada <kamiada528@gmail.com>
2022-05-18 19:38:28 +0100

    base template

--- characterCreator/settings.py
+++ characterCreator/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,4 +132,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "creator.apps.PagesConfig",  # new
+]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
