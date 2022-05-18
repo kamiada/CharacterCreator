d4d11516da688017e326f8875f967bc5ea63b942
kamiada <kamiada528@gmail.com>
2022-05-18 19:13:02 +0100

    hiding key

--- characterCreator/settings.py
+++ characterCreator/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$$lm(sllw)(=thh6yom)e5gnm4go3+&l+6ckpo_x$3u=rl$&=r'
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
