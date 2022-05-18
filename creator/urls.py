8563f352ef1fa032f351a06314f2c9026d059ab1
kamiada <kamiada528@gmail.com>
2022-05-18 18:33:05 +0100

    Bless it, works

--- creator/urls.py
+++ creator/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from .views import HomePageView
 urlpatterns = [
-    path('', views.index, name='index')
+    # path('', views.index, name='index')
+     path("", HomePageView.as_view(), name="home"),
 ]
--- creator/views.py
+++ creator/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def index(request):
-    return HttpResponse('Hello world!')
+# def index(request):
+#     return HttpResponse('Hello world!')
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
