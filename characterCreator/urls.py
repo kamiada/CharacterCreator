ac023a25a698b30e363a7a233a2435175ba04b8d
kamiada <kamiada528@gmail.com>
2022-05-18 17:43:52 +0100

    set up

--- characterCreator/urls.py
+++ characterCreator/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('charactercreator', include('creator.urls')),
+    path('character_creator/', include('creator.urls')),
     path('admin/', admin.site.urls),
 ]
