bbdacf45c13ae36ac1c39ab99150c140c7307d88
kamiada <kamiada528@gmail.com>
2022-05-19 11:07:45 +0100

    creator screen

--- creator/urls.py
+++ creator/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, CreatorPageView
 urlpatterns = [
     # path('', views.index, name='index')
+     path("creator/", CreatorPageView.as_view(), name="creator"),
      path("about/", AboutPageView.as_view(), name="about"),
      path("", HomePageView.as_view(), name="home"),
 ]
--- creator/views.py
+++ creator/views.py
@@ -10,3 +10,6 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+class CreatorPageView(TemplateView):
+    template_name = "creator.html"
